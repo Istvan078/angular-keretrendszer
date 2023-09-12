830b7598f825d767475b32ab95ed427332e2ce0f
Istvan078 <kalmaristvan078@gmail.com>
2023-09-12 20:30:34 +0200

    TourOfHeroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HŐSÖK: Hero[] = [
+    { id: 12, name: 'Dr. Zsivago' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Inarius' },
+    { id: 15, name: 'Magnus' },
+    { id: 16, name: 'Gumiember' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Magma' },
+    { id: 19, name: 'Dr. IQ' },
+    { id: 20, name: 'Tornado' }
+];
